98b5fa473ad9b5686c37281ca9caa8da6b45502f
lasse <77295879+lassejlv@users.noreply.github.com>
2024-10-05 22:14:51 +0200

    new build

--- probun.test.ts
+++ probun.test.ts
@@ -1,48 +1,48 @@
-import { test, expect } from "bun:test";
-import { Probun, logger } from "./src";
+import { test, expect } from 'bun:test';
+import { Probun, logger } from './src';
 
 let server: any;
 
-test("Server starts", () => {
+test('Server starts', () => {
   server = Probun({
     port: 3000,
-    routes: "routes",
+    routes: 'routes',
     onReady: ({ port }) => console.log(`Server is running on ${port}`),
     middlewares: [logger],
   });
   expect(server).toBeDefined();
 });
 
-test("GET request", async () => {
-  await fetch("http://localhost:3000").then((res) => {
+test('GET request', async () => {
+  await fetch('http://localhost:3000').then((res) => {
     expect(res.status).toBe(200);
   });
 });
 
-test("POST request", async () => {
-  await fetch("http://localhost:3000", {
-    method: "POST",
-    body: JSON.stringify({ name: "John" }),
+test('POST request', async () => {
+  await fetch('http://localhost:3000', {
+    method: 'POST',
+    body: JSON.stringify({ name: 'John' }),
   }).then(async (res) => {
     const resJ = await res.json();
-    expect(resJ).toEqual({ name: "John" });
+    expect(resJ).toEqual({ name: 'John' });
   });
 });
 
-test("PUT request", async () => {
-  await fetch("http://localhost:3000", {
-    method: "PUT",
+test('PUT request', async () => {
+  await fetch('http://localhost:3000', {
+    method: 'PUT',
   }).then(async (res) => {
     const resJ = await res.json();
-    expect(resJ).toEqual({ message: "PUT request" });
+    expect(resJ).toEqual({ message: 'PUT request' });
   });
 });
 
-test("DELETE request", async () => {
-  await fetch("http://localhost:3000", {
-    method: "DELETE",
+test('DELETE request', async () => {
+  await fetch('http://localhost:3000', {
+    method: 'DELETE',
   }).then(async (res) => {
     const resJ = await res.json();
-    expect(resJ).toEqual({ message: "DELETE request" });
+    expect(resJ).toEqual({ message: 'DELETE request' });
   });
 });
