c6ffc430edaa4f0720fd766d6c869702ea9bb304
Ben Thomas <benjamint08@gmail.com>
2024-10-05 20:54:09 +0100

    Probun tests (run bun test)

--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,18 @@
-import { Route } from '../lib';
+import { Route } from "../lib";
 
 export const GET = async (c: Route): Promise<Response> => {
-  return c.json({ message: 'Hello World' });
+  return c.json({ message: "Hello World" });
+};
+
+export const POST = async (c: Route): Promise<Response> => {
+  const body = await c.req.json();
+  return c.json(body);
+};
+
+export const PUT = async (c: Route): Promise<Response> => {
+  return c.json({ message: "PUT request" });
+};
+
+export const DELETE = async (c: Route): Promise<Response> => {
+  return c.json({ message: "DELETE request" });
 };
